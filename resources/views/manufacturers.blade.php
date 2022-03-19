94d503fce1bdb970dcfb5f3a1df5ee419c8151b3
Max <marzdor@gmail.com>
2022-03-18 20:33:25 -0700

    added base pages for tabs

---
+++ resources/views/manufacturers.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Manufacturers')
+
+@section('content_header')
+    <h1>Manufacturers</h1>
+@stop
+
+@section('content')
+    <p>Here.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/manufacturers', function () {
+    return view('manufacturers');
+});
+
+Route::get('/equipment', function () {
+    return view('equipment');
+});
