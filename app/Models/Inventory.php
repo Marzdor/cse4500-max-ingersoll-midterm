a20ac90d94aeb832009288f28731cbcf0a59c84c
Max <marzdor@gmail.com>
2022-03-20 10:48:03 -0700

    added uuid support

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
+    use Uuids;
     use HasFactory;
 
     protected $fillable = ['name'];
--- database/migrations/2022_03_20_035038_create_inventories_table.php
+++ database/migrations/2022_03_20_035038_create_inventories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('inventories', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name', 128);
             $table->timestamps();
         });
